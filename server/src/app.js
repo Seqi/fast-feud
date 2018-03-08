9b5d6cea45fe8aab4c480bc8e2340028051a8892
seqi <tomseqi@live.co.uk>
2018-03-08 21:09:17 +0000

    commit

--- server/src/app.js
+++ server/src/app.js
@@ -2,10 +2,21 @@ let express = require('express')
 let cors = require('cors')
 let app = express()
 
-app.use(cors())
+let socketio = require('socket.io')
+let http = require('http').createServer(app)
+let io = socketio(http)
+
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:3100'
+}))
 app.use('/food', require('./routes/yelp.routes'))
 
-let server = app.listen(process.env.PORT || 4500, () => {
+let server = http.listen(process.env.PORT || 4500, () => {
     let address = server.address()
     console.log(`Listening on ${address.address}:${address.port}`)
+})
+
+io.on('connection', socket => {
+    console.log('Connected')
 })
