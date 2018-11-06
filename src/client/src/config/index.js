5b1e375a645e00698c757932b6df5bcd89e38689
seqi <tomseqi@live.co.uk>
2018-11-06 20:32:03 +0000

    Fixed prod issue (temp..)

--- src/client/src/config/index.js
+++ src/client/src/config/index.js
@@ -1,3 +1,9 @@
-module.exports = process.env.NODE_ENV === 'production' ?
-	require('./config.prod') :
-	require('./config.dev')
+// module.exports = process.env.NODE_ENV === 'production' ?
+// 	require('./config.prod') :
+// 	require('./config.dev')
+
+// Temporary, lazy :(
+module.exports = {
+	apiUrl: '',
+	mapsApiKey: 'AIzaSyDrvmXGCzDXzP3-fPs2EEjYlv9IcXkof5U'
+}
