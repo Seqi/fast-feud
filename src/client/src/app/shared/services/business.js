ad16891a19932c7a0d5c5cc2d552de187e77f15b
seqi <tomseqi@live.co.uk>
2019-03-14 22:03:07 +0000

    Used relative api urls

--- src/client/src/app/shared/services/business.js
+++ src/client/src/app/shared/services/business.js
@@ -1,18 +1,18 @@
 import * as axios from 'axios'
-import config from '@config'
 
 let errorMessageMap = {
-	'LOCATION_MISSING': 'A location was not supplied. Please configure your location and try again.',
-	'LOCATION_NOT_FOUND': 'Could not find any restaurants with your provided location.',
-	'TOO_SMALL_VALIDATION_ERROR': 'A provided value was incorrect. Please check your options and try again.'
+	LOCATION_MISSING: 'A location was not supplied. Please configure your location and try again.',
+	LOCATION_NOT_FOUND: 'Could not find any restaurants with your provided location.',
+	TOO_SMALL_VALIDATION_ERROR: 'A provided value was incorrect. Please check your options and try again.'
 }
 
 export function loadBusiness(options) {
-	return axios
-		.get(`${config.apiUrl}/food/random?${buildQuery(options)}`)
-		.catch(error => {
-			throw new Error(errorMessageMap[error.response.data.error.code] || 'An error occurred. Please refresh and try again, reconfigure your options, or create a new room.')
-		})
+	return axios.get(`/food/random?${buildQuery(options)}`).catch(error => {
+		throw new Error(
+			errorMessageMap[error.response.data.error.code] ||
+				'An error occurred. Please refresh and try again, reconfigure your options, or create a new room.'
+		)
+	})
 }
 
 function buildQuery(options) {
--- src/client/src/config/config.prod.js
+++ src/client/src/config/config.prod.js
@@ -1,4 +1,3 @@
 module.exports = {
-	apiUrl: process.env.API_URL,
 	mapsApiKey: process.env.GOOGLEMAPS_API_KEY
 }
